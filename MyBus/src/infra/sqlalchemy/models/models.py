a70c65c0d73c34ffc8cec377d3fe621e29e16292
João Felipe <69603279+felipegt56@users.noreply.github.com>
2021-06-01 18:20:59 -0300

    models Passagem alterada

--- MyBus/src/infra/sqlalchemy/models/models.py
+++ MyBus/src/infra/sqlalchemy/models/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
-#from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 from src.infra.sqlalchemy.config.database import Base
 
 
@@ -12,10 +12,22 @@ class Usuario(Base):
     cpf = Column(String)
     telefone = Column(String)
 
+    passagens = relationship('Passagem', back_populates='usuario')
+
+class Passagem(Base):
+    __tablename__ = 'passagem'
+
+    id = Column(Integer, primary_key=True, index=True)
+    embarque = Column(String)
+    desembarque = Column(String)
+    dia_mes = Column(String)
+    usuario_id = Column(Integer, ForeignKey('usuario.id', name='fk_usuario'))
+
+    usuario = relationship('Usuario', back_populates='passagens')
+
 class Empresa(Base):
     __tablename__ = 'empresa'
 
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String)
     cnpj = Column(String)
-    
